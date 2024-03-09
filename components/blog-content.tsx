5919362fb76f42b38dff40eece357bebcb4747fd
dhk-code48 <dhk.code48@gmail.com>
2024-03-09 10:15:24 +0545

    Fixed blog content view error

--- components/blog-content.tsx
+++ components/blog-content.tsx
@@ -1,14 +1,17 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import FroalaEditorView from "react-froala-wysiwyg/FroalaEditorView";
 
 const BlogContent = ({ content }: { content: string }) => {
-  if (window !== undefined)
-    return (
-      <div className="pb-5 text-white">
-        <FroalaEditorView model={content} />
-      </div>
-    );
+  useEffect(() => {
+    console.log(content);
+  }, [content]);
+
+  return (
+    <div className="pb-5 text-white">
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+  );
 };
 
 export default BlogContent;
